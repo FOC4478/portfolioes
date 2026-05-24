2feb1c8b61f72f8baaebf20b8c4a6d39e6feece8
innocent okekem <iokekem@gmail.com>
2026-05-24 15:55:02 +0100

    made changes

--- index.js
+++ index.js
@@ -39,13 +39,6 @@ form.addEventListener("submit", function (e) {
 
     });
 
-new QRCode(document.getElementById("qrcode"), {
-    text: "https://personalportofoli.netlify.app/",
-    width: 180,
-    height: 180,
-    colorDark: "#00e0ff",
-    colorLight: "#ffffff",
-});
 
 
 });
